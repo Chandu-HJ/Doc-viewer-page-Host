16651b1b10380b49e06013ad9079a54fc617da95
Chandu-HJ <chanduhj1229@gmail.com>
2025-12-03 16:55:41 +0530

    updated

--- dist/components/doc-viewer.js
+++ dist/components/doc-viewer.js
@@ -1,4 +1,4 @@
-import { D as DocViewer$1, d as defineCustomElement$1 } from './p-738nk2jD.js';
+import { D as DocViewer$1, d as defineCustomElement$1 } from './p-QQXiyuF-.js';
 
 const DocViewer = DocViewer$1;
 const defineCustomElement = defineCustomElement$1;
